f97eb389abdd731ed7ad85e6ccd0edd58dd03fe9
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-06 21:41:38 +0100

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
