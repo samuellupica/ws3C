cfdf33979d233a9e716161c991fcc9cf9ee51977
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-27 22:31:53 +0100

    home page WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col justify-between">
+    <div class="flex flex-col">
+      <h1>Tierisch</h1>
+      <p>Mir geht es darum, dass ich Tieren und ihren Haltern helfen kann, wenn die Tiere Behindert sind oder werden.
+        Für
+        die Halter ist es oft eine neue, schwierige Situation, wobei ich unterstützend mit vielen Infos weiterhelfen
+        kann.</p>
+    </div>
+    <div class="flex flex-grow flex-1">
+      <h1>anders</h1>
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+
+</script>
