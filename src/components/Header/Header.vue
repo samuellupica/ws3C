9e1ae88bee7b79b14240ad596eb8e48bde361300
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-27 22:31:32 +0100

    header with navigation items

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="sticky absolute top-0" :style="`background-color: ${$route.meta.color}`">
+  <h3 @click="router.push('/')" class="text-center w-screen border-b border-yellowish-green py-3">
+    Tierisch anders
+  </h3>
+  <div class="lg:hidden">
+    <Navigation/>
+  </div>
+</div>
+</template>
+<script setup lang="ts">
+import router from "@/router";
+import Navigation from "@/components/Header/Navigation.vue";
+
+</script>
+<style>
+
+</style>
