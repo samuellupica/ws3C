186b38cab6e4a9d232bde0dcafa8780cb300b1ae
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-27 22:29:29 +0100

    update router, add new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ShopView from "../views/ShopView.vue";
+import ContactView from "../views/ContactView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +12,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/shop',
+      name: 'shop',
+      component: ShopView,
+      meta: {
+        color: '#8C8AE6',
+      }
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView,
+      meta: {
+        color: '#2D0077',
+      }
     }
   ]
 })
