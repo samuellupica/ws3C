19920325d2d18da5a9bec19d329544a88d093f07
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-28 18:54:33 +0100

    add FormContact.vue, FormInput.vue, FormTextArea.vue

---
+++ src/components/form/FormInput.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-text-field :label="inputLabel" variant="outlined" rounded="0" hide-details class="mb-2.5">
+  </v-text-field>
+</template>
+<script setup lang="ts">
+defineProps({
+  inputLabel: String,
+})
+</script>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
     <h2>Kontakt 联系</h2>
-    <p>
-      Bitte füllen Sie untenstehende Felder aus. Sie erhalten nach Absenden der Anfrage eine Bestätigung an Ihre Mail-Adresse. Ich werde mich bemühen, Ihre Frage oder Ihr Anliegen innert 48 Stunden*
+    <p class="mb-12">
+      Bitte füllen Sie untenstehende Felder aus. Sie erhalten nach Absenden der Anfrage eine Bestätigung an Ihre
+      Mail-Adresse. Ich werde mich bemühen, Ihre Frage oder Ihr Anliegen innert 48 Stunden*
       zu beantworten.
     </p>
+    <FormContact
+        first-name-label="Vorname"
+        last-name-label="Nachname"
+        email-label="E-Mail"
+    ></FormContact>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script>
+import {defineComponent} from "vue";
+import FormInput from "@/components/form/FormInput.vue";
+import FormContact from "@/components/form/FormContact.vue";
+
+export default defineComponent({
+  components: {FormContact, FormInput}
+})
+</script>
 
 <style scoped></style>
