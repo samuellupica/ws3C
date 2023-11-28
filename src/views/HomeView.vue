1664589399c95512c0f1b1ff704aaf4c74e54fcf
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-28 15:23:10 +0100

    set text-width equal to siblings width

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-col justify-between lg:flex-row" :style="{ height: contentHeight }">
+    <div class="flex flex-col justify-between lg:flex-row w-full" :style="{ height: contentHeight }">
       <div class="flex flex-col">
-        <h1>Tierisch</h1>
-        <p class="max-w-[450px]">Mir geht es darum, dass ich Tieren und ihren Haltern helfen kann, wenn die Tiere Behindert sind oder werden.
+        <h1 class="w-max" id="tierisch-heading-1">Tierisch</h1>
+        <p :style="{ maxWidth: textWidth}">Mir geht es darum, dass ich Tieren und ihren Haltern helfen kann, wenn die Tiere Behindert sind oder werden.
           Für
           die Halter ist es oft eine neue, schwierige Situation, wobei ich unterstützend mit vielen Infos weiterhelfen
           kann.</p>
@@ -15,18 +15,28 @@
 <script setup lang="js">
 import {onMounted, ref} from "vue";
 
-const contentHeight = ref('auto')
+const contentHeight = ref('auto');
+const textWidth = ref('auto');
+
+const updateWidth = () => {
+  textWidth.value = document.getElementById('tierisch-heading-1').offsetWidth + 'px';
+}
+
 const updateHeight = () => {
   const footerHeight = document.querySelector('footer').offsetHeight;
   const headerHeight = document.querySelector('header').offsetHeight;
   const clientHeight = document.documentElement.clientHeight;
   const contentElement = document.getElementById('content');
   const contentPadding = parseInt(window.getComputedStyle(contentElement).getPropertyValue('padding-top'));
-  contentHeight.value = (clientHeight - footerHeight - headerHeight - 2*contentPadding) + 'px'
+  contentHeight.value = (clientHeight - footerHeight - headerHeight - 2 * contentPadding) + 'px'
 }
 
 onMounted(() => {
   updateHeight();
-  window.addEventListener('resize', updateHeight)
+  updateWidth()
+  window.addEventListener(("resize"), () => {
+    updateWidth();
+    updateHeight();
+  })
 })
 </script>
