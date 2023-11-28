b2c296cfea2601332325843ba77f0fd6d3f7dc98
samuellupica <samuel.lupica@students.fhnw.ch>
2023-11-28 17:10:15 +0100

    add routerLinks and loop through all routes

--- src/components/Header/NavDesktop.vue
+++ src/components/Header/NavDesktop.vue
@@ -1,16 +1,27 @@
 <template>
   <nav class="max-lg:hidden pr-5 pl-5 top-0 gap-1 border-r" :style="{ minHeight: navHeight }">
     <div class="sticky top-24 flex justify-between desktop-navigation gap-8 h-1/2 overflow-auto">
-      <p>Shop</p>
-      <p>Contact</p>
-      <p>About us</p>
+      <RouterLink v-for="(item, index) in items" :key="index" :to="item.path">
+        {{ item.name }}
+      </RouterLink>
     </div>
   </nav>
 </template>
 <script setup lang="js">
-import { onMounted, ref } from 'vue'
+import {onMounted, ref} from 'vue'
+import router from "@/router";
 
 const navHeight = ref('auto')
+const items = ref([]);
+
+router.options.routes.forEach((route) => {
+  if (route.name !== 'home') {
+    items.value.push({
+      name: route.name,
+      path: route.path
+    })
+  }
+})
 
 const updateNavHeight = () => {
   const headerHeight = document.querySelector('header').offsetHeight
@@ -23,6 +34,7 @@ onMounted(() => {
   updateNavHeight()
   window.addEventListener('resize', updateNavHeight)
 })
+
 </script>
 <style>
 .desktop-navigation {
