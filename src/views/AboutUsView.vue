206065a839e0e6018ee7ba27504b67730df2c491
samuellupica <samuel.lupica@students.fhnw.ch>
2023-12-31 15:04:41 +0100

    adjust font sizes, change order AboutUsView.vue, format

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <h2>About us 关于</h2>
-    <div class="grid lg:grid-cols-5 gap-8">
-      <img src="/images/head-model.jpeg" loading="lazy" alt="About us" class="max-h-[400px] w-full object-cover aspect-[5/3] lg:col-span-3 invert" />
-      <p class="lg:col-span-2">
-        Willkommen bei TierischAnders – Ihrem Partner für einzigartige Mobilitätslösungen und stilvolle Accessoires für unsere tierischen Gefährten. Bei uns geht es nicht nur darum, das Leben von
-        Hunden und anderen Tieren zu erleichtern, sondern auch darum, ihre Individualität zu unterstreichen. Entdecken Sie unsere hochwertigen Rollstühle und exklusiven Accessoires, die nicht nur
-        funktional, sondern auch ein Statement sind. Erfahren Sie mehr über unsere Mission, unser Engagement und die Menschen, die hinter TierischAnders stehen. Zusammen schaffen wir eine Welt, in der
-        Tiere ihren ganz eigenen, besonderen Platz haben.
-      </p>
+    <div class="grid xl:grid-cols-5 gap-8">
+      <div class="xl:col-span-2">
+        <p class="text-[20px] font-bold mb-4">Willkommen bei TierischAnders – Ihrem Partner für einzigartige Mobilitätslösungen und stilvolle Accessoires für unsere tierischen Gefährten..</p>
+        <p>
+          Bei uns geht es nicht nur darum, das Leben von Hunden und anderen Tieren zu erleichtern, sondern auch darum, ihre Individualität zu unterstreichen. Entdecken Sie unsere hochwertigen
+          Rollstühle und exklusiven Accessoires, die nicht nur funktional, sondern auch ein Statement sind. Erfahren Sie mehr über unsere Mission, unser Engagement und die Menschen, die hinter
+          TierischAnders stehen. Zusammen schaffen wir eine Welt, in der Tiere ihren ganz eigenen, besonderen Platz haben.
+        </p>
+      </div>
+      <img src="/images/head-model.jpeg" loading="lazy" alt="About us" class="max-h-[400px] w-full object-cover aspect-[5/3] xl:col-span-3 invert" />
     </div>
   </div>
 </template>
@@ -32,5 +34,4 @@ onBeforeMount(() => {
     transform: scale(0.95);
   }
 }
-
 </style>
